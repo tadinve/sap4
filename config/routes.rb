0d7941c49b0a68211873b9bcd15c1ba4ff3712fd
Venkatesh Tadinada <venkat@solivar.com>
2012-03-11 16:04:12 -0700

    Finishing chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Sap4::Application.routes.draw do
+  get "users/new"
+
   get "static_pages/home"
 
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
 
+  root to: 'static_pages#home'
   match '/home' => 'static_pages#home'
+  match '/signup' => 'users#new'
+  match '/help' => 'static_pages#help'
+  match '/about' => 'static_pages#about'
+  match '/contact' => 'static_pages#contact'
+
 end
